2323c377ccb3e2069ad1a8751fea8e429f3e80ce
am3rco <am3rco@gmail.com>
2023-03-28 10:48:20 -0300

    Security

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -24,68 +24,69 @@ import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
-@RestController // Define que a classe é do tipo RestController e receberá requisições 
+@RestController // Define que a classe é do tipo RestController e receberá requisições
 @RequestMapping("/postagens")
-@CrossOrigin(origins = "*", allowedHeaders = "*") // Permite o recebimento de requisições realizadas fora do dominio
+@CrossOrigin(origins = "*", allowedHeaders = "*") // Permite o recebimento de requisições realizadas fora do dominio, ou
+													// seja, cada um fica em uma porta
 public class PostagemController {
 
 	@Autowired // Injeção de Dependência, aplicando a Inversão de Controle
 	private PostagemRepository postagemRepository;
-	
+
 	@Autowired
 	private TemaRepository temaRepository;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Postagem>> getAll(){
+	public ResponseEntity<List<Postagem>> getAll() {
 		return ResponseEntity.ok(postagemRepository.findAll());
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<Postagem> getByID(@PathVariable Long id) {
-		return postagemRepository.findById(id)
-				.map(resposta -> ResponseEntity.ok(resposta))
+		return postagemRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
-	
+
 	@GetMapping("/titulo/{titulo}")
-	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo) {
 		return ResponseEntity.ok(postagemRepository.findAllByTituloContainingIgnoreCase(titulo));
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
 		if (temaRepository.existsById(postagem.getTema().getId()))
-			return ResponseEntity.status(HttpStatus.CREATED)
-					.body(postagemRepository.save(postagem));
-		
+			return ResponseEntity.status(HttpStatus.CREATED).body(postagemRepository.save(postagem));
+
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 	}
-	
+
 	@PutMapping
 	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
 		if (postagemRepository.existsById(postagem.getId())) {
-			
+
 			if (temaRepository.existsById(postagem.getTema().getId()))
-				return ResponseEntity.status(HttpStatus.OK)
-						.body(postagemRepository.save(postagem));
-			
+				return ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem));
+
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-			
+
 		}
-		
+
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
 	}
-	
-	// Essa aplicação seria o equivalente a instrução: DELETE FROM tb_postagens WHERE id = id;.
-	@ResponseStatus(HttpStatus.NO_CONTENT) // Indica que quando a Resposta da requisição for positiva irá retornar No Content -> 204
+
+	// Essa aplicação seria o equivalente a instrução: DELETE FROM tb_postagens
+	// WHERE id = id;.
+	@ResponseStatus(HttpStatus.NO_CONTENT) // Indica que quando a Resposta da requisição for positiva irá retornar No
+											// Content -> 204
 	@DeleteMapping("/{id}")
-	public void delete(@PathVariable Long id) { // É do tipo void por retornar nenhum valor, uma vez que ao deletar uma postagem ela deixara de existir
+	public void delete(@PathVariable Long id) { // É do tipo void por retornar nenhum valor, uma vez que ao deletar uma
+												// postagem ela deixara de existir
 		Optional<Postagem> postagem = postagemRepository.findById(id);
-		
-		if(postagem.isEmpty()) 
+
+		if (postagem.isEmpty())
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
-			
-			postagemRepository.deleteById(id);
+
+		postagemRepository.deleteById(id);
 	}
-	
+
 }
